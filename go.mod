0341b80c62b9f5fdf17a0de2b2a0c62137d2146e
zachspang <spangzach@gmail.com>
2024-10-12 17:29:12 -0700

    Add ability to decode a video into frames

--- go.mod
+++ go.mod
@@ -3,10 +3,14 @@ module github.com/zachspang/mp4ToAscii
 go 1.23.2
 
 require (
+	github.com/aws/aws-sdk-go v1.55.5 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/kopoli/go-terminal-size v0.0.0-20170219200355-5c97524c8b54 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/u2takey/ffmpeg-go v0.5.0 // indirect
+	github.com/u2takey/go-utils v0.3.1 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 )
--- mp4ToAscii/cmd/root.go
+++ mp4ToAscii/cmd/root.go
@@ -1,11 +1,15 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"image/jpeg"
+	"io"
 	"os"
 
 	tsize "github.com/kopoli/go-terminal-size"
 	"github.com/spf13/cobra"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
 var rootCmd = &cobra.Command{
@@ -13,8 +17,28 @@ var rootCmd = &cobra.Command{
 		var tSize tsize.Size
 
 		tSize, err := tsize.GetSize()
-		if err == nil {
-			fmt.Println("Current size is", tSize.Width, "by", tSize.Height)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		fmt.Println("Current terminal size is", tSize.Width, "by", tSize.Height)
+
+		img, err := jpeg.Decode(ReadFrameAsJpeg("..\\input.mp4", 1))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		file, err := os.Create("..\\output.jpeg")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		err = jpeg.Encode(file, img, &jpeg.Options{Quality: 100})
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
 		}
 	},
   }
@@ -25,3 +49,16 @@ var rootCmd = &cobra.Command{
 	  os.Exit(1)
 	}
   }
+
+  func ReadFrameAsJpeg(inFileName string, frameNum int) io.Reader {
+	buf := bytes.NewBuffer(nil)
+	err := ffmpeg.Input(inFileName).
+		Filter("select", ffmpeg.Args{fmt.Sprintf("gte(n,%d)", frameNum)}).
+		Output("pipe:", ffmpeg.KwArgs{"loglevel":"warning", "vframes": 1, "format": "image2", "vcodec": "mjpeg"}).
+		WithOutput(buf, os.Stdout).
+		Run()
+	if err != nil {
+		panic(err)
+	}
+	return buf
+}
