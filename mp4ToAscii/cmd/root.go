64f9362a4c78aa3dfb529ea5d4a8da235abe5e93
zachspang <spangzach@gmail.com>
2024-10-13 00:42:42 -0700

    Add feature to convert frames into ASCII and display the first frame in the console

--- mp4ToAscii/cmd/root.go
+++ mp4ToAscii/cmd/root.go
@@ -27,11 +27,22 @@ var rootCmd = &cobra.Command{
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-		fmt.Println("Current terminal size is", tSize.Width, "by", tSize.Height)
 
 		//Get the size and total frames of the input video
 		originalWidth, originalHeight, frameCount := GetVideoInfo(filename)
 
+		//Set the new width and height based on the terminal size
+		newHeight := 0
+		newWidth := 0
+		
+		if tSize.Width / 2 > tSize.Height{
+			newHeight = tSize.Height
+			newWidth = (int((float32(newHeight) / float32(originalHeight)) * float32(originalWidth)))
+		} else{
+			newWidth = tSize.Width / 2
+			newHeight = (int((float32(newWidth) / float32(originalWidth)) * float32(originalHeight)))
+		}
+		
 		var frames []image.Image
 		//Read all frames, decode them from jpeg, then append them to the slice frames
 		for frameIndex := 0; frameIndex < frameCount; frameIndex++ {
@@ -45,12 +56,34 @@ var rootCmd = &cobra.Command{
 
 		//For each frame make a blank image of the new size and then draw over it
 		for frameIndex, frame := range frames{
-			resizedImage:= image.NewRGBA(image.Rect(0, 0, originalWidth/10, originalHeight/10))
+			resizedImage:= image.NewRGBA(image.Rect(0, 0, newWidth, newHeight))
 			//Add flag for different quality options https://pkg.go.dev/golang.org/x/image/draw#pkg-variables
 			draw.CatmullRom.Scale(resizedImage, resizedImage.Rect, frame, frame.Bounds(), draw.Over, nil)
 			frames[frameIndex] = resizedImage
 		}
-		
+
+		//Slice with the ASCII representation for a pixel. Each frame is stored as a string with escape sequences for color and newlines
+		var asciiList[]string
+		//For each pixel in each frame get the relative luminance in a range of 0-255, select a char based on that, and set an ANSI color
+		for frameIndex, frame := range frames{
+			asciiList = append(asciiList, "")
+			for y := range newHeight{
+				for x := range newWidth{
+					R,G,B,_ := frame.At(x,y).RGBA()
+					red := uint8(R>>8)
+					green := uint8(G>>8)
+					blue := uint8(B>>8)
+					//relativeLuminance := (0.2126 * float32(R)) + (0.715 * float32(G)) + (0.0722 * float32(B))
+					ansi := "\x1b[38;2;" + strconv.FormatUint(uint64(red), 10) + ";" + strconv.FormatUint(uint64(green), 10) + ";" + strconv.FormatUint(uint64(blue), 10) + "m" 
+					ansi += "██"
+					asciiList[frameIndex] += ansi
+				}
+				asciiList[frameIndex] += "\033[0m\n"
+			}
+		}
+		//Print the frame
+		fmt.Println(asciiList[0])
+		fmt.Println("\033[0m")
 		//DELETE LATER. Save the first frame for testing purposes
 			img, _ := jpeg.Decode(ReadFrameAsJpeg(filename, 0))
 			file, _ := os.Create("..\\output.jpeg")
@@ -72,8 +105,9 @@ func ReadFrameAsJpeg(inFileName string, frameNum int) io.Reader {
 	buf := bytes.NewBuffer(nil)
 	err := ffmpeg.Input(inFileName).
 		Filter("select", ffmpeg.Args{fmt.Sprintf("gte(n,%d)", frameNum)}).
-		Output("pipe:", ffmpeg.KwArgs{"loglevel":"warning", "vframes": 1, "format": "image2", "vcodec": "mjpeg"}).
-		WithOutput(buf, os.Stdout).
+		Output("pipe:", ffmpeg.KwArgs{"loglevel":"quiet", "vframes": 1, "format": "image2", "vcodec": "mjpeg"}).
+		WithOutput(buf).
+		Silent(true).
 		Run()
 	if err != nil {
 		panic(err)
